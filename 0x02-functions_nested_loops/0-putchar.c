655b07238d3ef4b0eea9a77bede57f377bbab0a9
David Agbaniyaka <fiyin.agba@gmail.com>
2022-12-13 05:42:44 -0800

    First Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Write a program that prints _putchar, followed by a new line.
- * Return - Always return 0.
+ * Return: Always return 0
  */
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
