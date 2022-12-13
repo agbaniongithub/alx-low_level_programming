1accc33becdb0763919ce853b938057699946d20
David Agbaniyaka <fiyin.agba@gmail.com>
2022-12-13 15:10:15 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value
+ * @t: integer
+ * Return: absolute value of the integer
+ */
+
+int _abs(int t)
+{
+if (t >= 0)
+return (t);
+else
+return (-t);
+}
