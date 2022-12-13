bc85c8c8721060b79c8c2abe057729cbf8e528f5
David Agbaniyaka <fiyin.agba@gmail.com>
2022-12-13 09:26:37 -0800

    third task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -prints alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+int ten;
+char alphabet;
+for (ten = 0; ten <= 9; ten++)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+_putchar('\n');
+}
