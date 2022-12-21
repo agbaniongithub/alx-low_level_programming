6668a7ae8a0478fa0b7324fd20665e96ba10d542
David Agbaniyaka <fiyin.agba@gmail.com>
2022-12-21 15:23:06 -0800

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - function for copying strings
+ * @dest: parameter1
+ * @src: parameter2
+ * @n: parameter3
+ * Return: String
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+strncpy(dest, src, n);
+return (dest);
+}
