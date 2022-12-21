3a62027086ef95940c3770cef05dc01bea7debb7
David Agbaniyaka <fiyin.agba@gmail.com>
2022-12-21 15:04:05 -0800

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - function that concatenates two strings.
+ * @dest: variable 1
+ * @src: variable 2
+ * @n: variable 3
+ * Return: String
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+strncat(dest, src, n);
+return (dest);
+}
+
