e7576a02ae5eef6acd483ec261889a6622d9ebd9
David Agbaniyaka <fiyin.agba@gmail.com>
2022-12-21 15:37:36 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,8 +8,14 @@
  * Return: String
  */
 
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
-strncpy(dest, src, n);
+int index = 0, src_len = 0;
+while (src[index++])
+src_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[index] = src[index];
+for (index = src_len; index < n; index++)
+dest[index] = '\0';
 return (dest);
 }
