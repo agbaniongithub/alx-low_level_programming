629305f8d0b6a6b68c99ffce69f568f949e97b24
David Agbaniyaka <fiyin.agba@gmail.com>
2023-02-21 00:18:06 -0800

    If else while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-
 /**
-* main - Entry point
+* main - Prints the alphabet.
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-int n;
-int lastD;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-lastD = n % 10;
-if (lastD > 5)
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, lastD);
+putchar(alp[i]);
 }
-else if (lastD == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, lastD);
-}
-else
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastD);
-}
+putchar('\n');
 return (0);
 }
