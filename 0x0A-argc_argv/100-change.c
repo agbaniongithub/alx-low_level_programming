43b84e2275847fa0690e3bed29ccd47a8ef50eb5
David Agbaniyaka <fiyin.agba@gmail.com>
2023-02-20 20:49:26 -0800

    100-change.c correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
-* main - prints the minimum number of coins to
-* make change for an amount of money
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: 0 (Success), 1 (Error)
+* main - print min number of coins to return change
+* @argc: argument count
+* @argv: argument vector
+* Return: 1 if error, 0 if success
 */
 int main(int argc, char *argv[])
 {
-int num, j, result;
-int coins[] = {25, 10, 5, 2, 1};
+int cents, remainder, coins;
+int quarters, dimes, nickles, twos, pennies;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-num = atoi(argv[1]);
-result = 0;
-if (num < 0)
+else
+cents = atoi(argv[1]);
+if (cents <= 0)
 {
 printf("0\n");
 return (0);
 }
-for (j = 0; j < 5 && num >= 0; j++)
-{
-while (num >= coins[j])
-{
-result++;
-num -= coins[j];
-}
-}
-printf("%d\n", result);
+quarters = cents / 25;
+remainder = cents % 25;
+dimes = remainder / 10;
+remainder = remainder % 10;
+nickles = remainder / 5;
+remainder = remainder % 5;
+twos = remainder / 2;
+remainder = remainder % 2;
+pennies = remainder / 1;
+coins = quarters + dimes + nickles + twos + pennies;
+printf("%d\n", coins);
 return (0);
+}
